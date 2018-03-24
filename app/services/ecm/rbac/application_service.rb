eb6019e72e1506177b4217858770732428362f20
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-24 19:04:01 +0100

    Use Rails::AddOns::Service::Base as base service.

---
+++ app/services/ecm/rbac/application_service.rb
@@ -0,0 +1,8 @@
+module Ecm
+  module Rbac
+    class ApplicationService < Rails::AddOns::Service::Base
+      class Result < Rails::AddOns::Service::Result::Base
+      end
+    end
+  end
+end
--- lib/ecm_rbac.rb
+++ lib/ecm_rbac.rb
@@ -1,2 +1,4 @@
+require 'rails-add_ons'
+
 require 'ecm/rbac'
 require 'ecm/rbac/engine'
