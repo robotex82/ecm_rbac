8879be902716d8602d6ecfe343611dfa1c74aaf5
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-06-26 18:31:11 +0200

    Rescue NameError when trying to include Model::Ecm::Rbac::UserConcern into User.

--- config/initializers/inject_user_concern.rb
+++ config/initializers/inject_user_concern.rb
@@ -1,6 +1,10 @@
 Rails.configuration.to_prepare do
-  puts "Including Model::Ecm::Rbac::UserConcern in #{Ecm::Rbac.user_class_name}"
-  Ecm::Rbac.user_class_name.constantize.class_eval do
-    include Model::Ecm::Rbac::UserConcern
+  begin
+    puts "Including Model::Ecm::Rbac::UserConcern in #{Ecm::Rbac.user_class_name}"
+    Ecm::Rbac.user_class_name.constantize.class_eval do
+      include Model::Ecm::Rbac::UserConcern
+    end
+  rescue NameError => e
+    "Could not include Model::Ecm::Rbac::UserConcern into #{Ecm::Rbac.user_class_name}. Error: #{e.message}"
   end
 end
